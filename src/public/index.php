092b172b841ba1c261ce47bd6dedf11176941b44
guomangu <gyomang@gmail.com>
2026-04-12 15:25:55 +0200

    reprise de avril 3 endo 3

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Prevent PHP 8.5 deprecation warnings (e.g., PDO::MYSQL_ATTR_SSL_CA) from breaking HTTP responses
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
